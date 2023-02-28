a8eb9b23a7b663089aa85466a9f257a463e72458
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2023-02-28 11:28:29 +0900

    절대경로 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import Home from "./features/home/Container";
+import "index.css";
+import reportWebVitals from "reportWebVitals";
+import Home from "features/home/Container";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
