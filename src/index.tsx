6a5eddb21cd7376e7b8c3362381cdebd81926212
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2023-03-04 14:46:18 +0900

    GlobalStyle 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "index.css";
 import reportWebVitals from "reportWebVitals";
 
 import App from "App";
+
+import GlobalStyle from "GlobalStyle";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,6 +15,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <GlobalStyle />
     </BrowserRouter>
   </React.StrictMode>
 );
