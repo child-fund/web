6afeb062f0e3755e89caafc84d08b3542959eda7
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2023-03-04 16:12:40 +0900

    화면 최대 너비, 높이 설정

--- src/App.style.tsx
+++ src/App.style.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  // 최대 넓이와 높이 설정해줘야 할듯
-  min-width: 100vw;
-  min-height: 100vh;
+  max-width: 41.2rem;
+  max-height: 91.5rem;
 `;
