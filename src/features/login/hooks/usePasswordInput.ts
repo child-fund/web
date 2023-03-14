635211deb21d0991700a3034b28d32c885a7196c
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2023-03-14 09:30:11 +0900

    [ESC-4] login 페이지 UI 생성

---
+++ src/features/login/hooks/usePasswordInput.ts
@@ -0,0 +1,36 @@
+import { ChangeEvent, useState } from "react";
+
+const usePasswordInput = () => {
+  const [password, setPassword] = useState("");
+  const [passwordWrongInput, setPasswordWrongInput] = useState(false);
+  const [passwordWarningMessage, setPasswordWarningMessage] = useState("");
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+
+    const result = checkValidity(e.target.value);
+
+    if (result) {
+      setPasswordWrongInput(false);
+    } else {
+      setPasswordWrongInput(true);
+      setPasswordWarningMessage("이 비밀번호가 아닌거 같아요 :(");
+    }
+  };
+
+  const checkValidity = (value: string) => {
+    // TODO: api 들어갈 자리
+    const pattern =
+      /^(?!((?:[A-Za-z]+)|(?:[~!@#$%^&*()_+=]+)|(?:[0-9]+))$)[A-Za-z\d~!@#$%^&*()_+=]{8,19}$/;
+    return pattern.test(value);
+  };
+
+  return {
+    handlePasswordChange,
+    password,
+    passwordWarningMessage,
+    passwordWrongInput,
+  };
+};
+
+export default usePasswordInput;
