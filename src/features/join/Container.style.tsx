1290b8496beb235214353629baf4ef0b3b8f2ac9
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2023-03-22 20:22:06 +0900

    [ESC-14] join 페이지 하단 안내 영역 sticky 적용

--- src/features/join/Container.style.tsx
+++ src/features/join/Container.style.tsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 import Title from "shared/components/Title/Container";
 import Description from "shared/components/Description/Container";
-import Announcement from "shared/components/Announcement/Container";
+
+import colors from "shared/assets/colors";
 
 export const Container = styled.div`
   position: relative;
@@ -13,9 +14,16 @@ export const Container = styled.div`
   }
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   height: 100%;
-  padding: 2rem;
-  padding-top: 2.5rem;
+  padding: 2.5rem 2rem 0rem 2rem;
+`;
+
+export const ContentAreaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
 
 export const StyledTitle = styled(Title)`
@@ -26,9 +34,18 @@ export const StyledDescription = styled(Description)`
   margin-bottom: 3.6rem;
 `;
 
-export const StyledAnnouncement = styled(Announcement)`
-  position: absolute;
-  left: 50%;
-  bottom: 2rem;
-  transform: translateX(-50%);
+export const AnnouncementAreaContainer = styled.div`
+  position: sticky;
+  bottom: 0rem;
+  width: calc(100% + 4rem);
+  padding: 2.4rem 0rem 2rem 0rem;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(255, 255, 255, 0) 9.5%,
+    ${colors.white} 25%
+  );
+
+  > div {
+    flex-grow: 1;
+  }
 `;
--- src/features/join/Container.tsx
+++ src/features/join/Container.tsx
@@ -3,12 +3,14 @@ import useJoin from "./hooks/useJoin";
 import Button, { ButtonTheme } from "shared/components/Button/Container";
 import InputArea from "shared/components/InputArea/Container";
 import Input from "shared/components/Input/Container";
+import Announcement from "shared/components/Announcement/Container";
 
 import {
   Container,
   StyledTitle,
   StyledDescription,
-  StyledAnnouncement,
+  ContentAreaContainer,
+  AnnouncementAreaContainer,
 } from "./Container.style";
 
 const JoinContainer = () => {
@@ -36,61 +38,65 @@ const JoinContainer = () => {
 
   return (
     <Container>
-      <StyledTitle
-        text={`1분만 시간내어서
+      <ContentAreaContainer>
+        <StyledTitle
+          text={`1분만 시간내어서
         회원가입을 부탁드려요`}
-      />
-      <StyledDescription text="회원가입 후에 바로 기부참여가 가능해요" />
-      <InputArea>
-        <Input
-          placeholder="닉네임을 하나 만들어볼까요?"
-          // placeholder="한글 혹은 영문으로 만들어주세요"
-          onValueChange={handleNicknameChange}
-          title="닉네임"
-          value={nickname}
-          wrongInput={nicknameWrongInput}
-          warningMessage={nicknameWarningMessage}
         />
-        <Input
-          placeholder="사용할 아이디를 만들어주세요!"
-          onValueChange={handleIdChange}
-          title="아이디"
-          value={id}
-          wrongInput={idWrongInput}
-          warningMessage={idWarningMessage}
+        <StyledDescription text="회원가입 후에 바로 기부참여가 가능해요" />
+        <InputArea>
+          <Input
+            placeholder="닉네임을 하나 만들어볼까요?"
+            // placeholder="한글 혹은 영문으로 만들어주세요"
+            onValueChange={handleNicknameChange}
+            title="닉네임"
+            value={nickname}
+            wrongInput={nicknameWrongInput}
+            warningMessage={nicknameWarningMessage}
+          />
+          <Input
+            placeholder="사용할 아이디를 만들어주세요!"
+            onValueChange={handleIdChange}
+            title="아이디"
+            value={id}
+            wrongInput={idWrongInput}
+            warningMessage={idWarningMessage}
+          />
+          <Input
+            placeholder="8자 이상 20자 이하로 영문, 숫자를 조합해야해요!"
+            onValueChange={handlePasswordChange}
+            title="비밀번호"
+            type="password"
+            value={password}
+            wrongInput={passwordWrongInput}
+            warningMessage={passwordWarningMessage}
+          />
+          <Input
+            placeholder="만든 비밀번호를 확인해볼까요?"
+            onValueChange={handleRetypedPasswordChange}
+            title="비밀번호 확인"
+            type="password"
+            value={retypedPassword}
+            wrongInput={retypedPasswordWrongInput}
+            warningMessage={retypePasswordWarningMessage}
+          />
+        </InputArea>
+        {canSubmit && (
+          <Button
+            onButtonClick={handleSubmitClick}
+            text="기부 동참하러 가기"
+            theme={ButtonTheme.DARK}
+          />
+        )}
+      </ContentAreaContainer>
+      <AnnouncementAreaContainer>
+        <Announcement
+          buttonText="로그인 하러가기"
+          leftText="이미 가입했다면? "
+          onButtonClick={handleLoginClick}
+          rightText="를 클릭해주세요!"
         />
-        <Input
-          placeholder="8자 이상 20자 이하로 영문, 숫자를 조합해야해요!"
-          onValueChange={handlePasswordChange}
-          title="비밀번호"
-          type="password"
-          value={password}
-          wrongInput={passwordWrongInput}
-          warningMessage={passwordWarningMessage}
-        />
-        <Input
-          placeholder="만든 비밀번호를 확인해볼까요?"
-          onValueChange={handleRetypedPasswordChange}
-          title="비밀번호 확인"
-          type="password"
-          value={retypedPassword}
-          wrongInput={retypedPasswordWrongInput}
-          warningMessage={retypePasswordWarningMessage}
-        />
-      </InputArea>
-      {canSubmit && (
-        <Button
-          onButtonClick={handleSubmitClick}
-          text="기부 동참하러 가기"
-          theme={ButtonTheme.DARK}
-        />
-      )}
-      <StyledAnnouncement
-        buttonText="로그인 하러가기"
-        leftText="이미 가입했다면? "
-        onButtonClick={handleLoginClick}
-        rightText="를 클릭해주세요!"
-      />
+      </AnnouncementAreaContainer>
     </Container>
   );
 };
