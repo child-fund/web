0cbeb61c61383c7bfa6f9f85afdd73172ab7fbb1
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2023-03-27 10:04:55 +0900

    [ESC-16] 메인 페이지 공지사항 color 정정

--- src/features/main/Components/NoticeSection/Container.style.tsx
+++ src/features/main/Components/NoticeSection/Container.style.tsx
@@ -5,10 +5,12 @@ export const Container = styled.ul`
   width: 100%;
 
   > li {
+    color: ${colors.gray500};
     font-size: 1rem;
     line-height: 1.8rem;
 
     > span {
+      color: ${colors.gray800};
       font-size: 1rem;
       font-weight: 600;
     }
