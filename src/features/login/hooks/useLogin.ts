769c19af9f088e620e4665bdb08996026728b379
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2023-04-02 22:37:30 +0900

    [ESC-18] 로그인 후 참여여부에 따라 페이지 이동 다르게 설정

--- src/features/login/hooks/useLogin.ts
+++ src/features/login/hooks/useLogin.ts
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import { ToastContext } from "shared/components/Toast/ToastProvider";
 import postSignIn from "../apis/postSignIn";
+import getParticipation from "shared/apis/getParticipation";
 
 import useAccountIdInput from "./useAccountIdInput";
 import usePasswordInput from "./usePasswordInput";
@@ -34,14 +35,31 @@ const useLogin = () => {
       localStorage.setItem("accessToken", data.accessToken);
       localStorage.setItem("nickname", data.nickname); // TODO: 전역변수로 with jotai
 
-      // TODO: 참여여부에 따라 경로 다르게
-      navigate("/");
-      // navigate("/history");
+      await checkParticipationAndMoveNext();
       return;
     }
 
     showToast(`이용량 급증으로 인해 로그인이 지연되고 있어요.
     이 메시지가 반복된다면 1688-4272 고객센터로 연락주세요.`);
+  };
+
+  const checkParticipationAndMoveNext = async () => {
+    // TODO: accessToken 저장하는거랑 getParticipation이랑 묶을 수 있을것 같은데, 그럼 getParticipation의 에러 toast는 어떻게 보여주지? useContext를 못쓰는데. 꼭 manager로 만들필요 없고 hook으로 해도 될까?
+    const { result, data } = await getParticipation();
+
+    if (!result || !data) {
+      showToast(`에러가 발생했어요.
+      이 메시지가 반복된다면 1688-4272 고객센터로 연락주세요.`);
+      return;
+    }
+
+    if (data.isParticipate) {
+      navigate("/history");
+    } else {
+      navigate("/");
+    }
+
+    localStorage.setItem("participated", String(data.isParticipate));
   };
 
   const allInputValidated = [
