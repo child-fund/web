b8515688aca50d19129a837e6e5a24bc0f4dab4f
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2023-04-04 20:11:58 +0900

    [ESC-21] 종이비행기 선택화면의 이미지 파일/파일명 교체

--- src/features/donationCertificate/Container.tsx
+++ src/features/donationCertificate/Container.tsx
@@ -20,7 +20,7 @@ import {
 import escalPrintSeal from "shared/assets/imgs/escalPrintSeal.png";
 import escalPrintLogo from "shared/assets/imgs/escalPrintLogo.png";
 import childFundLogo from "shared/assets/imgs/childFundLogo.png";
-import paperAirplaneGreen from "shared/assets/imgs/paperAirplaneGreen.png";
+import selectPaperAirplaneGreen from "shared/assets/imgs/selectPaperAirplaneGreen.png";
 
 const DonationCertificateContainer = () => {
   const {
@@ -41,7 +41,10 @@ const DonationCertificateContainer = () => {
         기부에 동참해주셔서 감사해요 :)`}</Title>
           <Description>{`꿈이 이루어질 수 있도록 에스칼프린트가 함께할게요!
         저소득층 기부에 함께 동참해주셔서 너무 고마워요!`}</Description>
-          <AirplaneImage src={paperAirplaneGreen} alt="paperAirplaneGreen" />
+          <AirplaneImage
+            src={selectPaperAirplaneGreen}
+            alt="selectPaperAirplaneGreen"
+          />
           <Phrase>위 자는 저소득층 기부에 동참하였음을 인증합니다.</Phrase>
           <Signature>
             <span>에스칼디렉션 대표인</span>
--- src/features/selectAirplane/constants/airplaneList.ts
+++ src/features/selectAirplane/constants/airplaneList.ts
@@ -1,13 +1,13 @@
 import AirplaneKey from "../types/airplaneKey";
 
-import paperAirplaneGreen from "shared/assets/imgs/paperAirplaneGreen.png";
-import paperAirplaneBeige from "shared/assets/imgs/paperAirplaneBeige.png";
-import paperAirplaneSky from "shared/assets/imgs/paperAirplaneSky.png";
-import paperAirplanePink from "shared/assets/imgs/paperAirplanePink.png";
-import paperAirplanePurple from "shared/assets/imgs/paperAirplanePurple.png";
-import paperAirplaneYellowGreen from "shared/assets/imgs/paperAirplaneYellowGreen.png";
-import paperAirplaneRed from "shared/assets/imgs/paperAirplaneRed.png";
-import paperAirplaneOrange from "shared/assets/imgs/paperAirplaneOrange.png";
+import selectPaperAirplaneGreen from "shared/assets/imgs/selectPaperAirplaneGreen.png";
+import selectPaperAirplaneBeige from "shared/assets/imgs/selectPaperAirplaneBeige.png";
+import selectPaperAirplaneSky from "shared/assets/imgs/selectPaperAirplaneSky.png";
+import selectPaperAirplanePink from "shared/assets/imgs/selectPaperAirplanePink.png";
+import selectPaperAirplanePurple from "shared/assets/imgs/selectPaperAirplanePurple.png";
+import selectPaperAirplaneYellowGreen from "shared/assets/imgs/selectPaperAirplaneYellowGreen.png";
+import selectPaperAirplaneRed from "shared/assets/imgs/selectPaperAirplaneRed.png";
+import selectPaperAirplaneOrange from "shared/assets/imgs/selectPaperAirplaneOrange.png";
 
 export interface Airplane {
   key: AirplaneKey;
@@ -21,49 +21,49 @@ const airplaneList: Airplane[] = [
     key: "green",
     containerBackground: `linear-gradient(to bottom, #fff 21%, #64cc7f 90%, #2caf45)`,
     previewBorder: `linear-gradient(225deg, #58d78d 99%, #41d359 53%, #62b14f 2%)`,
-    image: paperAirplaneGreen,
+    image: selectPaperAirplaneGreen,
   },
   {
     key: "beige",
     containerBackground: `linear-gradient(to bottom, #fff 21%, #c8cc64 90%, #ccc625)`,
     previewBorder: `linear-gradient(225deg, #cbd05d 99%, #e5e054 53%, #ccc625 2%)`,
-    image: paperAirplaneBeige,
+    image: selectPaperAirplaneBeige,
   },
   {
     key: "sky",
     containerBackground: `linear-gradient(to bottom, #fff 21%, #8ddbed 90%, #2bacc9)`,
     previewBorder: `linear-gradient(225deg, #6dd0e6 99%, #62c6dd 53%, #2bacc9 2%)`,
-    image: paperAirplaneSky,
+    image: selectPaperAirplaneSky,
   },
   {
     key: "pink",
     containerBackground: `linear-gradient(to bottom, #fff 21%, #ffaaf1 90%, #db5dd4)`,
     previewBorder: `linear-gradient(225deg, #f47ee0 99%, #ff88f8 53%, #db5dd4 2%);`,
-    image: paperAirplanePink,
+    image: selectPaperAirplanePink,
   },
   {
     key: "purple",
     containerBackground: `linear-gradient(to bottom, #fff 21%, #d78ded 90%, #802bc9)`,
     previewBorder: `linear-gradient(225deg, #b467dd 99%, #b762dd 53%, #742bc9 2%)`,
-    image: paperAirplanePurple,
+    image: selectPaperAirplanePurple,
   },
   {
     key: "yellowGreen",
     containerBackground: `linear-gradient(to bottom, #fff 21%, #a0ffa7 90%, #36e544)`,
     previewBorder: `linear-gradient(225deg, #77ef81 99%, #78fa82 53%, #68cb44 2%)`,
-    image: paperAirplaneYellowGreen,
+    image: selectPaperAirplaneYellowGreen,
   },
   {
     key: "red",
     containerBackground: `linear-gradient(to bottom, #fff 21%, #ed8d8d 90%, #c92b2b)`,
     previewBorder: `linear-gradient(225deg, #e56b6b 99%, #dd6262 53%, #c92b2b 2%)`,
-    image: paperAirplaneRed,
+    image: selectPaperAirplaneRed,
   },
   {
     key: "orange",
     containerBackground: `linear-gradient(to bottom, #fff 21%, #ffd8aa 90%, #db9c5d)`,
     previewBorder: `linear-gradient(225deg, #eacc6a 99%, #eabb54 53%, #dbaf5d 2%)`,
-    image: paperAirplaneOrange,
+    image: selectPaperAirplaneOrange,
   },
 ];
 
