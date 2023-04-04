71e9cc0cc8890def44061990f61d3d3ecd38c8b9
Yunjeong Choi <m.yunjeong.choi@gmail.com>
2023-04-04 21:52:01 +0900

    [ESC-21] 기부목록 공지사항 클릭 핸들러 추가

--- src/features/donationHistory/hooks/useDonationHistory.ts
+++ src/features/donationHistory/hooks/useDonationHistory.ts
@@ -1,7 +1,11 @@
 const useDonationHistory = () => {
   const handleLoadMoreClick = () => {};
 
-  const handleNoticeClick = () => {};
+  const handleNoticeClick = () => {
+    window.open(
+      "https://escal-print.notion.site/X-25392410e8b04c10b17b66d7cd413b54"
+    );
+  };
 
   return {
     handleLoadMoreClick,
